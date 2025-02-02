8a77c23bf445a49e912c3bc7a470aeeab8462660
Ashointay <gerasho63@gmail.com>
2025-02-03 01:47:09 +0500

    c6

---
+++ classes_6.py
@@ -0,0 +1,13 @@
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+user_input = (input())
+numbers = list(map(int, user_input.split()))
+prime_numbers = list(filter(lambda x: is_prime(x), numbers))
+
+print(prime_numbers)
